bc13d5bf8378bbb0a90fd3f9cfcd6896d2acdaa0
Pecepla <sectores.xvii@gmail.com>
2025-03-25 13:46:00 +0100

    segundo commit

--- Airport/src/main/java/com/airport/Airport/Controller/PlaneController.java
+++ Airport/src/main/java/com/airport/Airport/Controller/PlaneController.java
@@ -1,63 +1,14 @@
 package com.airport.Airport.Controller;
 
 
-import com.airport.Airport.Model.Plane;
-import com.airport.Airport.Service.PlaneService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/planes")
 public class PlaneController {
 
-    @Autowired
-    private PlaneService planeService;
 
-
-
-
-
-
-    @GetMapping("/{id}")
-    public ResponseEntity<Plane> getPlaneById(@PathVariable Integer id) {
-        return planeService.findById(id)
-                .map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
-    }
-
-
-
-    @PostMapping
-    public Plane createPlane(@RequestBody Plane plane) {
-        return planeService.savePlane(plane);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<Plane> updatePlane(@PathVariable Integer id, @RequestBody Plane  plane){
-        return planeService.findById(id)
-                .map(existingPlane -> {
-                    plane.setId(id);
-                    return ResponseEntity.ok(planeService.savePlane(plane));
-                })
-                .orElse(ResponseEntity.notFound().build());
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletePlane(@PathVariable Integer id) {
-        return planeService.findById(id)
-                .map(plane  -> {
-                    planeService.deleteById(id);
-                    return ResponseEntity.ok().<Void>build();
-                })
-                .orElse(ResponseEntity.notFound().build());
-    }
-
-    @GetMapping("/search")
-    public List<Plane> searchPlane(@RequestParam String keyword) {
-        return planeService.searchPlane(keyword);
-
-    }
 
 
 
