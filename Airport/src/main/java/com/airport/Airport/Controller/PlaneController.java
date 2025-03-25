f91ea860c2cec3438fc58e7497f239779da47d97
Pecepla <sectores.xvii@gmail.com>
2025-03-25 13:53:53 +0100

    commit arreglos

--- Airport/src/main/java/com/airport/Airport/Controller/PlaneController.java
+++ Airport/src/main/java/com/airport/Airport/Controller/PlaneController.java
@@ -2,11 +2,23 @@ package com.airport.Airport.Controller;
 
 
 
+import com.airport.Airport.Model.Plane;
+import com.airport.Airport.Service.PlaneService;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/planes")
 public class PlaneController {
+
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Plane> getWordById(@PathVariable Long id) {
+        Optional<Plane> word = PlaneService.getPlaneById(id);
 
 
 
--- Airport/src/main/java/com/airport/Airport/Service/PlaneService.java
+++ Airport/src/main/java/com/airport/Airport/Service/PlaneService.java
@@ -1,12 +1,63 @@
 package com.airport.Airport.Service;
 
 
+import com.airport.Airport.Model.Plane;
+import com.airport.Airport.Repository.PlaneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PlaneService {
 
+    @Autowired
+    private PlaneRepository PlaneRepository;
 
+    public List<Plane> getAllPlane() {
+        return PlaneRepository.findAll();
+    }
+
+    public Optional<AppUser> getUserById(String id) {
+        return appUserRepository.findById(id);
+    }
+
+    public AppUser createUser(AppUser user) {
+        return appUserRepository.save(user);
+    }
+
+    public AppUser updateUser(AppUser user) {
+        return appUserRepository.save(user);
+    }
+
+    public void deleteUser(String id) {
+        appUserRepository.deleteById(id);
+    }
+
+    public void deleteAllUsers() {
+        appUserRepository.deleteAll();
+    }
+
+    public boolean existsById(String id) {
+        return appUserRepository.existsById(id);
+    }
+
+    public long getUserCount() {
+        return appUserRepository.count();
+    }
+
+    // Additional business logic can be added here
+    public List<AppUser> getActiveUsers() {
+        return appUserRepository.findByIsActiveTrue();
+    }
+
+    public AppUser getUserByEmail(String email) {
+        return appUserRepository.findByEmail(email);
+    }
+
+    public List<AppUser> getUsersByAgeGreaterThan(int age) {
+        return appUserRepository.findByAgeGreaterThan(age);
+    }
 
 
 }
